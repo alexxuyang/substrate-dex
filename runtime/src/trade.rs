54e7c2363e727f5c149d0883aa13d21421047dec
xuyang <yang.xu@nbltrust.com>
2019-08-26 10:57:29 +0800

    add test case structure

--- runtime/src/trade.rs
+++ runtime/src/trade.rs
@@ -354,3 +354,82 @@ impl<T: Trait> Module<T> {
 		Ok(())
 	}
 }
+
+/// tests for this module
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use runtime_io::with_externalities;
+	use primitives::{H256, Blake2Hasher};
+	use support::{impl_outer_origin, assert_ok};
+	use runtime_primitives::{
+		BuildStorage,
+		traits::{BlakeTwo256, IdentityLookup},
+		testing::{Digest, DigestItem, Header}
+	};
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	// For testing the module, we construct most of a mock runtime. This means
+	// first constructing a configuration type (`Test`) which `impl`s each of the
+	// configuration traits of modules we want to use.
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type Digest = Digest;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = ();
+		type Log = DigestItem;
+	}
+
+	impl balances::Trait for Test {
+		type Balance = u128;
+
+		type OnFreeBalanceZero = ();
+
+		type OnNewAccount = ();
+
+		type Event = ();
+
+		type TransactionPayment = ();
+		type DustRemoval = ();
+		type TransferPayment = ();
+	}
+
+	impl token::Trait for Test {
+		type Event = ();
+	}
+
+	impl Trait for Test {
+		type Event = ();
+		type Price = u64;
+	}
+
+	type TradeModule = Module<Test>;
+
+	// This function basically just builds a genesis storage key/value store according to
+	// our desired mockup.
+	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+		system::GenesisConfig::<Test>::default().build_storage().unwrap().0.into()
+	}
+
+	#[test]
+	fn it_works_for_default_value() {
+		with_externalities(&mut new_test_ext(), || {
+			let ok: Result = Ok(());
+			assert_ok!(ok);
+
+			assert_eq!(1u32, 1);
+		});
+	}
+}
