434d6f3504894ac671beaa0dd8ac2b46102deb06
alexxuyang <alexxuyang@qq.com>
2019-12-27 17:19:01 +0800

    add utils.rs

---
+++ runtime/src/utils.rs
@@ -0,0 +1,15 @@
+use rstd::if_std;
+
+if_std! {
+    pub struct ByteBuf<'a>(pub &'a [u8]);
+    
+    impl<'a> std::fmt::LowerHex for ByteBuf<'a> {
+        fn fmt(&self, fmtr: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+            for byte in self.0 {
+                fmtr.write_fmt(format_args!("{:02x}", byte))?;
+            }
+            Ok(())
+        }
+    }
+}
+
